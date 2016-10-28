b51c220f595cfb6f9033ae96f3d15dfa65000034
Matt Forbes <mforbes@twitter.com>
2016-10-27 17:37:38 -0700

    tweak

--- models.py
+++ models.py
@@ -200,19 +200,19 @@ class SimpleModel(BaseModel):
         """
         """
         model = Sequential()
-        model.add(Convolution2D(16, 4, 4,
+        model.add(Convolution2D(16, 5, 5,
             input_shape=input_shape,
             init= "glorot_uniform",
             activation='relu',
             border_mode='same',
             bias=True))
-        model.add(MaxPooling2D(pool_size=(4, 4)))
+        model.add(MaxPooling2D(pool_size=(4, 5)))
         model.add(Convolution2D(32, 4, 4,
             init= "glorot_uniform",
             activation='relu',
             border_mode='same',
             bias=True))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(MaxPooling2D(pool_size=(2, 3)))
         model.add(Convolution2D(64, 3, 3,
             init= "glorot_uniform",
             activation='relu',
