2a06d0fcd86fcfbe74d90c00a55acd0cb2cf8607
Matt Forbes <mforbes@twitter.com>
2016-10-31 15:46:37 -0700

    update submission code

--- submission.py
+++ submission.py
@@ -1,7 +1,7 @@
 """
 Evaluate the final test set.
 """
-import os
+import os, time
 
 import cv2
 import numpy as np
@@ -10,12 +10,11 @@ from scipy.stats.mstats import mquantiles
 from datasets import load_dataset
 from models import load_from_config
 
-#if __name__ == '__main__':
 def main():
     images_path = '/media/drive/Challenge 2/Test/center'
     input_shape = (120, 320, 3)
 
-    model_type = 'ensemble'
+    model_type = 'regression'
 
     if model_type == 'ensemble':
         final_model_config = {
@@ -41,43 +40,28 @@ def main():
             'model_uri': 's3://sdc-matt/lstm/1477805095/lstm.h5',
             'timesteps': 9
         }
-    elif model_type == 'simple':
+    elif model_type == 'regression':
         final_model_config = {
-            'type': 'simple',
-            'model_uri': 's3://sdc-matt/simple/1477715388/model.h5',
-            'cat_classes': 5,
+            'type': 'regression',
+            'model_uri': 's3://sdc-matt/regression/1477947323/model.h5',
         }
+
     model = load_from_config(final_model_config)
 
     if model_type == 'ensemble':
         predictor = model.make_stateful_predictor(True)
-    if model_type == 'lstm':
-        def predictor(x):
-            return model.predict_on_batch(x)[0][0]
-    elif model_type == 'simple':
-        dataset = load_dataset(
-            's3://sdc-matt/datasets/final_training')
-        training_labels = dataset.get_training_labels()
-        prob = np.arange(0, 1 + 1./5, 1. / 5)
-        bins = mquantiles(training_labels, prob)
-        cat_values = []
-        for i in xrange(len(bins) - 1):
-            cond = (
-                (training_labels >= bins[i])
-                & (training_labels < bins[i+1]))
-            cat_values.append(training_labels[np.where(cond)].mean())
+    elif model_type in ('lstm', 'regression'):
+        predictor = lambda x: model.predict_on_batch(x)[0][0]
 
-        def predictor(x):
-            pred_cats = model.predict_on_batch(
-                x.reshape((1,) + input_shape))[0]
-            return cat_values[np.argmax(pred_cats)]
     model.model.summary()
 
-    for filename in sorted(os.listdir(images_path)):
-        src = os.path.join(images_path, filename)
-        x = load_test_image(src).reshape((1, ) + input_shape)
-        y_pred = predictor(x)
-        print '%s,%s' % (filename, y_pred)
+    with open('submission.%s.csv' % int(time.time()), 'w') as f:
+        f.write('frame_id,steering_angle\n')
+        for filename in sorted(os.listdir(images_path)):
+            src = os.path.join(images_path, filename)
+            x = load_test_image(src).reshape((1, ) + input_shape)
+            y_pred = predictor(x)
+            f.write('%s,%s\n' % (filename, y_pred))
 
 def load_test_image(src):
     cv_image = cv2.imread(src)
