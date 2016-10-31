1a19980ccb562b6d0188bb148a83a5febd6ac5aa
Nishanth Alapati <nish@cs.ucla.edu>
2016-10-31 20:31:30 +0000

    Generate submissions based on lstm model

--- submission.py
+++ submission.py
@@ -12,34 +12,49 @@ from models import load_from_config
 
 #if __name__ == '__main__':
 def main():
-    images_path = '/sdc/Challenge 2/Test/center'
+    images_path = '/media/drive/Challenge 2/Test/center'
     input_shape = (120, 320, 3)
 
-    simple = False
+    model_type = 'ensemble'
 
-    final_model_config = {
-        'type': 'ensemble',
-        'input_model_config': {
-            'model_uri': 's3://sdc-matt/simple/1477715388/model.h5',
-            'type': 'simple',
-            'cat_classes': 5,
-        },
-        'model_uri': 's3://sdc-matt/ensemble/1477725706/ensemble.h5',
-        'timesteps': 3,
-        'timestep_noise': 0.1,
-        'timestep_dropout': 0.5,
-    }
-
-    if simple:
+    if model_type == 'ensemble':
+        final_model_config = {
+            'type': 'ensemble',
+            'input_model_config': {
+                'model_uri': 's3://sdc-matt/simple/1477715388/model.h5',
+                'type': 'simple',
+                'cat_classes': 5,
+            },
+            'model_uri': 's3://sdc-matt/ensemble/1477725706/ensemble.h5',
+            'timesteps': 3,
+            'timestep_noise': 0.1,
+            'timestep_dropout': 0.5,
+        }
+    elif model_type == 'lstm':
+        final_model_config = {
+            'type': 'lstm',
+            'input_model_config': {
+                'model_uri': 's3://sdc-matt/simple/1477715388/model.h5',
+                'type': 'simple',
+                'cat_classes': 5,
+            },
+            'model_uri': 's3://sdc-matt/lstm/1477805095/lstm.h5',
+            'timesteps': 9
+        }
+    elif model_type == 'simple':
         final_model_config = {
             'type': 'simple',
             'model_uri': 's3://sdc-matt/simple/1477715388/model.h5',
             'cat_classes': 5,
         }
-
     model = load_from_config(final_model_config)
 
-    if simple:
+    if model_type == 'ensemble':
+        predictor = model.make_stateful_predictor(True)
+    if model_type == 'lstm':
+        def predictor(x):
+            return model.predict_on_batch(x)[0][0]
+    elif model_type == 'simple':
         dataset = load_dataset(
             's3://sdc-matt/datasets/final_training')
         training_labels = dataset.get_training_labels()
@@ -56,10 +71,6 @@ def main():
             pred_cats = model.predict_on_batch(
                 x.reshape((1,) + input_shape))[0]
             return cat_values[np.argmax(pred_cats)]
-
-    else:
-        predictor = model.make_stateful_predictor(True)
-
     model.model.summary()
 
     for filename in sorted(os.listdir(images_path)):
