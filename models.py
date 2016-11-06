fbda46f8486795b61fdfd088b37908ce21da7827
Matt Forbes <mforbes@twitter.com>
2016-11-06 15:36:16 -0800

    glorot -> he

--- models.py
+++ models.py
@@ -154,19 +154,19 @@ class CategoricalModel(BaseModel):
         model = Sequential()
         model.add(Convolution2D(16, 5, 5,
             input_shape=input_shape,
-            init= "glorot_uniform",
+            init= "he_normal",
             activation='relu',
             border_mode='same',
             bias=True))
         model.add(MaxPooling2D(pool_size=(4, 5)))
         model.add(Convolution2D(32, 4, 4,
-            init= "glorot_uniform",
+            init= "he_normal",
             activation='relu',
             border_mode='same',
             bias=True))
         model.add(MaxPooling2D(pool_size=(2, 3)))
         model.add(Convolution2D(64, 3, 3,
-            init= "glorot_uniform",
+            init= "he_normal",
             activation='relu',
             border_mode='same',
             bias=True))
@@ -175,13 +175,13 @@ class CategoricalModel(BaseModel):
         model.add(Dropout(0.5))
         model.add(Dense(
             output_dim=64,
-            init='glorot_uniform',
+            init='he_normal',
             activation='relu',
             bias=True))
         model.add(Dropout(0.5))
         model.add(Dense(
             output_dim=(1 + len(thresholds)),
-            init='glorot_uniform',
+            init='he_normal',
             W_regularizer=l2(W_l2),
             activation='sigmoid'))
 
@@ -276,17 +276,17 @@ class RegressionModel(BaseModel):
         model = Sequential()
         model.add(Convolution2D(16, 5, 5,
             input_shape=input_shape,
-            init= "glorot_uniform",
+            init= "he_normal",
             activation='relu',
             border_mode='same'))
         model.add(MaxPooling2D(pool_size=(4, 5)))
         model.add(Convolution2D(32, 4, 4,
-            init= "glorot_uniform",
+            init= "he_normal",
             activation='relu',
             border_mode='same'))
         model.add(MaxPooling2D(pool_size=(2, 3)))
         model.add(Convolution2D(64, 3, 3,
-            init= "glorot_uniform",
+            init= "he_normal",
             activation='relu',
             border_mode='same'))
         model.add(MaxPooling2D(pool_size=(2, 2)))
@@ -294,12 +294,12 @@ class RegressionModel(BaseModel):
         model.add(Dropout(0.5))
         model.add(Dense(
             output_dim=64,
-            init='glorot_uniform',
+            init='he_normal',
             activation='relu'))
         model.add(Dropout(0.5))
         model.add(Dense(
             output_dim=1,
-            init='glorot_uniform',
+            init='he_normal',
             W_regularizer=l2(W_l2)))
 
         optimizer = ('adadelta' if use_adadelta
@@ -491,14 +491,14 @@ class EnsembleModel(BaseModel):
                 input_dim=input_dim,
                 output_dim=output_dim,
                 activation='relu',
-                init='glorot_uniform',
+                init='he_normal',
                 bias=True,
                 W_regularizer=l2(W_l2)))
             model.add(Dropout(0.5))
 
         model.add(Dense(
             output_dim=1,
-            init='glorot_uniform',
+            init='he_normal',
             W_regularizer=l2(W_l2)))
 
         model.compile(loss=loss, optimizer=sgd, metrics=metrics)
@@ -644,7 +644,7 @@ class LstmModel(BaseModel):
         model.add(LSTM(256))
         model.add(Dense(
             output_dim=1,
-            init='glorot_uniform',
+            init='he_normal',
             W_regularizer=l2(W_l2)))
 
         model.compile(loss=loss, optimizer=sgd, metrics=metrics)
